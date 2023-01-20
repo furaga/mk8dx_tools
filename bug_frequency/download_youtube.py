32a9ca690d97d3ced2bc80a57b8f563f6d93e4d3
furaga <furaga.fukahori@gmail.com>
2023-01-21 05:47:02 +0900

    wip

--- bug_frequency/download_youtube.py
+++ bug_frequency/download_youtube.py
@@ -1,5 +1,25 @@
 from pytube import YouTube
 
+
+from pathlib import Path
+
+
+def download_videos(video_urls, person_name):
+    import re
+    for k, urls in video_urls.items():
+        for url in urls:
+            stream = YouTube(url).streams.filter(
+                file_extension='mp4').order_by('resolution').desc().first()
+            print("Downloading", url, stream.title)
+            out_dir = "E:/prog/python/mk8dx_tools/videos/" + person_name + "/" + k
+            Path(out_dir).mkdir(exist_ok=True, parents=True)
+            fname = stream.title + "-" + (url.split('/')[-1])
+            fname = re.sub(r'[\\|/|:|?|.|"|<|>|\|]', '', fname)
+            fname += ".mp4"
+            print("Saving", out_dir, "|", fname)
+            stream.download(out_dir, fname)
+
+
 # # 岸堂天真さん
 # video_urls = [
 #     'https://youtu.be/2T9CSdLG4Uc',
@@ -39,7 +59,6 @@ from pytube import YouTube
 
 
 # 　https://www.youtube.com/watch?v=nzkk2hCIVdo&ab_channel=%E3%83%96%E3%83%A9%E3%83%83%E3%82%AD%E3%83%BC
-
 # ブラッキーさん
 video_urls = {
     # "DLC3": [
@@ -56,6 +75,14 @@ video_urls = {
     #     "https://youtu.be/g0eY9QtyLPE",
     #     "https://youtu.be/e3b8op8IvtU",
     # ],
+    "DLC2": [
+        "https://youtu.be/zsL5n1IFU68",
+        "https://youtu.be/QEpltEr0q4k",
+        "https://youtu.be/YJWD_8gEKXs",
+        "https://youtu.be/1fg5ZpZsagM",
+        "https://youtu.be/53w-2r_KqbY",
+        "https://youtu.be/Sh2z7_xQ8GU",
+    ],
     # "DLC1": [
     #     "https://youtu.be/W9jHYnFrfzc",
     #     "https://youtu.be/SAvl3fT0mrA",
@@ -63,26 +90,27 @@ video_urls = {
     #     "https://youtu.be/7-wos2ZnFtQ",
     #     "https://youtu.be/BKZdgAR-vEg",
     # ],
-    "DLC0": [
-        "https://youtu.be/fZdBFzbT6aU",
-        "https://youtu.be/5pChjtll3mo",
-        "https://youtu.be/WHrVlKHFFCQ",
-        "https://youtu.be/6ZxshMoF31Q",
-        "https://youtu.be/S4Wb_3Ey0eU",
-        "https://youtu.be/69XUTaAdpOE",
-        "https://youtu.be/1ztAJrB1GQE",
-        "https://youtu.be/cCHJutf_PgI",
-        "https://youtu.be/wG8IlC2fiaQ",
-        "https://youtu.be/-U8P4xIGymY",
-        "https://youtu.be/DGn9u5jBVDA",
-        "https://youtu.be/CoiTdnxICSc",
-        "https://youtu.be/qgYGBwHqY5s",
-    ],
+    # "DLC0": [
+    #     "https://youtu.be/fZdBFzbT6aU",
+    #     "https://youtu.be/5pChjtll3mo",
+    #     "https://youtu.be/WHrVlKHFFCQ",
+    #     "https://youtu.be/6ZxshMoF31Q",
+    #     "https://youtu.be/S4Wb_3Ey0eU",
+    #     "https://youtu.be/69XUTaAdpOE",
+    #     "https://youtu.be/1ztAJrB1GQE",
+    #     "https://youtu.be/cCHJutf_PgI",
+    #     "https://youtu.be/wG8IlC2fiaQ",
+    #     "https://youtu.be/-U8P4xIGymY",
+    #     "https://youtu.be/DGn9u5jBVDA",
+    #     "https://youtu.be/CoiTdnxICSc",
+    #     "https://youtu.be/qgYGBwHqY5s",
+    # ],
 }
 
+download_videos(video_urls, "person3")
 
 # シェリンさん
-#video_urls = {
+video_urls = {
     # "DLC3": [
     #     "https://youtu.be/xERDoA3mgIw",
     #     "https://youtu.be/VNLlwQSJSfE",
@@ -97,6 +125,13 @@ video_urls = {
     #     "https://youtu.be/MmaKrHaR9k0",
     #     "https://youtu.be/Pji7YOP8XLI",
     # ],
+    "DLC2": [
+        "https://youtu.be/uTxIxTwB0Wg",
+        "https://youtu.be/c0isk0voHF0",
+        "https://youtu.be/oJ7U3GQGgJ8",
+        "https://youtu.be/mMPN5gRXR-c",
+        "https://youtu.be/_71RHmtzTWQ",
+    ],
     # "DLC1": [
     #     "https://youtu.be/vZLwTcXkbB4",
     #     "https://youtu.be/QDM3tvKEvOY",
@@ -110,18 +145,9 @@ video_urls = {
     #     "https://youtu.be/DbdzGSB3R6Y",
     #     "https://youtu.be/7pJUn-jVYvY",
     #     "https://youtu.be/iKb_qD6oFvc",
-        # "https://youtu.be/aGOVGK3ZEuY",
-        # "https://youtu.be/wJ_Y-0BW8XM",
+    # "https://youtu.be/aGOVGK3ZEuY",
+    # "https://youtu.be/wJ_Y-0BW8XM",
     # ]
-#}
+}
 
-from pathlib import Path
-for k, urls in video_urls.items():
-    for url in urls:
-        stream = YouTube(url).streams.filter(
-            file_extension='mp4').order_by('resolution').desc().first()
-        print("Downloading", url, stream.title)
-        out_dir = "E:/prog/python/mk8dx_tools/videos/person3/" + k
-        if not Path(out_dir).exists():
-            Path(out_dir).mkdir(exist_ok=True, parents=True)
-        stream.download(out_dir, stream.title + "-" + (url.split('/')[-1]) + ".mp4")
+download_videos(video_urls, "person1")
